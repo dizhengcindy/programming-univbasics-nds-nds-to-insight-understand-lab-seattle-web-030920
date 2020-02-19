2edbe62b737675a4302f2af013463b012be633d7
Di Zheng <dizhengcindy@gmail.com>
2020-02-19 22:04:24 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -6,19 +6,8 @@ def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp nds
 end
-
-pp directors_database
  
 def print_first_directors_movie_titles
-    col = 0 
-    while col<directors_database[0].length do
-      index = 0 
-      inner_len = directors_database[0][col].length
-      while index < inner_len do
-       puts "#{directors_database[0][col][index]}"
-       index += 1
-      end
-      col +=1
-    end
+   
     
 end
